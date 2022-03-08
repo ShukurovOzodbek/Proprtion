d0dd45860e1d79f65ddda29dad6abe101859cb24
ShukurovOzodbek <shukurov.ozodbek.jaloliddinovich@gmail.com>
2022-03-08 19:36:56 +0500

    my_proprtion

---
+++ script.js
@@ -0,0 +1,17 @@
+function prop1(  ){
+    let b = +prompt('Write: b')
+    let c = +prompt('Write: c')
+    alert(c / 100 * b)
+}
+
+function prop2(  ){
+    let d = +prompt('Write: d')
+    let b = +prompt('Write: b')
+    alert((d / b * 100).toFixed(1))
+}
+
+function prop3(  ){
+    let c = +prompt('Write: c')
+    let d = +prompt('Write: d')
+    alert(((d / c) * 100).toFixed(1))
+}
